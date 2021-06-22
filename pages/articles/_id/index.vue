85df97102e073d1c98a3aad4b7f30b18b783544e
新田陸人 <>
2021-06-22 13:21:20 +0900

    Nuxt Linkを設置

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>記事詳細ページ</div>
+  <div>
+    <div>記事詳細ページ</div>
+    <div>
+      <nuxt-link to="/articles/10/edit">Nuxt Link</nuxt-link>
+    </div>
+    <div>
+      <a href="/articles/10/edit">a タグ</a>
+    </div>
+  </div>
 </template>
 
 <script>
