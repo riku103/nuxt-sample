cff992c5310d69a6dc960c7a9a0d193c66de9309
新田陸人 <>
2021-06-23 14:07:10 +0900

    iShowを使い要素の表示、非表示を切り替え

---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-show="isShown">みえるかな？</div>
+    <v-btn @click="toggleShown">表示/非表示の切り替え</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShown: true,
+    }
+  },
+  methods: {
+    toggleShown() {
+      this.isShown = !this.isShown
+    },
+  },
+}
+</script>
