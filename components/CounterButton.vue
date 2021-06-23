ba7948acaaa15ed011add9c0e56669354a9a99f7
新田陸人 <>
2021-06-23 12:56:17 +0900

    親コンポーネントから子コンポーネントへ値を渡す

--- components/CounterButton.vue
+++ components/CounterButton.vue
@@ -1,5 +1,20 @@
 <template>
-  <v-btn class="mx-1" fab dark color="blue" @click="plus">
-    <v-icon dark> mdi-plus </v-icon>
+  <v-btn class="mx-1" fab dark :color="color" @click="plus">
+    <v-icon dark> mdi-{{ kind }} </v-icon>
   </v-btn>
 </template>
+
+<script>
+export default {
+  props: {
+    kind: {
+      type: String,
+      default: 'plus',
+    },
+    color: {
+      type: String,
+      default: 'blue',
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="container">
     <div class="number">{{ count }}</div>
-    <v-btn class="mx-1" fab dark color="blue" @click="plus">
+    <!-- <v-btn class="mx-1" fab dark color="blue" @click="plus">
       <v-icon dark> mdi-plus </v-icon>
     </v-btn>
     <v-btn class="mx-1" fab dark color="orange" @click="minus">
       <v-icon dark> mdi-minus </v-icon>
-    </v-btn>
-    <counter-button />
+    </v-btn> -->
+    <counter-button kind="plus" color="blue" />
+    <counter-button kind="minus" color="orange" />
   </div>
 </template>
 
