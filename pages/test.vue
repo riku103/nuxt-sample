b3e915f96f31c63bb36f544018100814568e25c4
新田陸人 <>
2021-06-23 14:46:00 +0900

    v-if を使った条件分岐

--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <div v-show="isShown">みえるかな？</div>
-    <v-btn @click="toggleShown">表示/非表示の切り替え</v-btn>
+    <!-- v-if とその仲間たち -->
+    <div v-if="score >= 80">よくできました</div>
+    <div v-else-if="score >= 30">もっといける！！</div>
+    <div v-else>がんばろう！</div>
+
+    <!-- v-show -->
+    <!-- <div v-show="isShown">みえるかな？</div> -->
+    <!-- <v-btn @click="toggleShown">表示/非表示の切り替え</v-btn> -->
   </div>
 </template>
 
@@ -10,6 +16,7 @@ export default {
   data() {
     return {
       isShown: true,
+      score: 90,
     }
   },
   methods: {
