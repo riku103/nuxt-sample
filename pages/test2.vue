cca4404cdf6508ab250f8a14156d85d9ce2df3db
新田陸人 <>
2021-06-24 13:10:43 +0900

    各ライフサイクルフックで処理が止まることを確認

--- pages/test2.vue
+++ pages/test2.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
     <div>テスト</div>
+
+    <v-text-field v-model="number" />
+    <div>数値: {{ number }}</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      number: 0,
+    }
+  },
+
+  created() {
+    console.log('created')
+  },
+
+  mounted() {
+    console.log('mounted')
+  },
+
+  updated() {
+    console.log('updated')
+  },
+}
 </script>
 
 <style lang="scss" module></style>
