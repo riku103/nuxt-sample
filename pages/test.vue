a5a4a7c303c8e52b0315b291dab962986d13f65b
新田陸人 <>
2021-06-24 11:10:11 +0900

    v-bind でメソッドを指定

--- pages/test.vue
+++ pages/test.vue
@@ -5,11 +5,8 @@
     <div v-else-if="score >= 30">もっといける！！</div>
     <div v-else>がんばろう！</div>
 
-    <a :href="googleLink">Googleへのリンク</a>
-    <img
-      src="https://2.bp.blogspot.com/-zDsrPiqkR_0/U0pTNj7lpoI/AAAAAAAAfGA/2Z1onz10tAw/s800/grade2_yokudekimashita.png"
-      alt="よくできました"
-    />
+    <!-- <a :href="googleLink">Googleへのリンク</a> -->
+    <img :src="imageUrl()" width="100px" />
 
     <!-- v-show -->
     <!-- <div v-show="isShown">みえるかな？</div> -->
@@ -22,13 +19,22 @@ export default {
   data() {
     return {
       isShown: true,
-      score: 90,
+      score: 10,
       googleLink: 'https://google.com',
     }
   },
   methods: {
     toggleShown() {
       this.isShown = !this.isShown
+    },
+    imageUrl() {
+      if (this.score >= 80) {
+        return 'https://2.bp.blogspot.com/-zDsrPiqkR_0/U0pTNj7lpoI/AAAAAAAAfGA/2Z1onz10tAw/s800/grade2_yokudekimashita.png'
+      } else if (this.score >= 30) {
+        return 'https://2.bp.blogspot.com/-wJDmO2MIKkg/U0pTOEZT-JI/AAAAAAAAfGc/p9uwt9Msq9A/s800/grade3_ganbarimashita.png'
+      } else {
+        return 'https://2.bp.blogspot.com/-Bc8NTQ2PqY8/U0pTOuIDWQI/AAAAAAAAfGY/1RU2LKdozKs/s800/grade5_ganbarimasyou.png'
+      }
     },
   },
 }
